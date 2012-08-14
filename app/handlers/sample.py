9e0131c3dcc06d4aaded0ac141e9cc006f0aa192
Logan Capaldo <logancapaldo@gmail.com>
2012-08-14 22:48:04 +0000

    Rework the forwarding code
    The state machine thing is for multiple emails,
    not like "Are you a subscriber?"
    
    Also I was doing the sending the message wrong

--- app/handlers/sample.py
+++ app/handlers/sample.py
@@ -1,40 +1,40 @@
 import logging
 from lamson.routing import route, route_like, stateless
 from config.settings import relay
-from lamson import view
+from lamson import view, mail
 from app.model import subscribers
 
 
-@route("(address)@(host)", address=".+")
+@route("lameserv@example.com")
 def START(message, address=None, host=None):
-    return NEW_USER
-
-
-@route_like(START)
-def NEW_USER(message, address=None, host=None):
-    return NEW_USER
-
-
-@route_like(START)
-def END(message, address=None, host=None):
-    return NEW_USER(message, address, host)
-
-
-@route_like(START)
-@stateless
-def FORWARD(message, address=None, host=None):
     subs = subscribers.Subscribers()
     if not subs.is_subscriber(message["From"]):
         return
 
     for sub in subs:
-        message['To'] = sub
-        message['From'] = "lameserv@lameserv.net"
-        relay.deliver(message)
+        resp = mail.MailResponse(sub, "lameserv@example.com", message["Subject"], message.body())
+        resp.attach_all_parts(message)
+        
+        relay.deliver(resp.to_message())
 
-@route_like(START)
-@stateless
-def CHECK_SUB(message, address=None, host=None):
-    if "subscribe" in message.body():
-        logging.debug("%s@%s wants to subscribe" % (address, host))
 
+#@route_like(START)
+#def NEW_USER(message, address=None, host=None):
+#    return NEW_USER
+#
+#
+#@route_like(START)
+#def END(message, address=None, host=None):
+#    return NEW_USER(message, address, host)
+#
+#
+#@route_like(START)
+#@stateless
+#def FORWARD(message, address=None, host=None):
+#
+#@route_like(START)
+#@stateless
+#def CHECK_SUB(message, address=None, host=None):
+#    if "subscribe" in message.body():
+#        logging.debug("%s@%s wants to subscribe" % (address, host))
+#
