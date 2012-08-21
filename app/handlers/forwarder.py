cc4b8facd15a0cc2efa1fe7d35586b5bb4c86dc3
Logan Capaldo <logancapaldo@gmail.com>
2012-08-21 01:37:12 +0000

    Increment the counter in replies in place
    Re: [st 1] Re: [st 2] is not how groups does it
    Do [st 1], Re: [st 2] instead.

--- app/handlers/forwarder.py
+++ app/handlers/forwarder.py
@@ -1,8 +1,17 @@
 import logging
+import re
 from lamson.routing import route, route_like, stateless
 from config.settings import relay, lameserv_endpoint_address
 from lamson import view, mail
+from lamson.server import SMTPError
 from app.model import subscribers, threadcounter
+
+def build_subject(count, original_subject):
+    m = re.search(r"^(Re:\s+)\[st (\d+)\](.*)", original_subject, re.I)
+    if m is None:
+        return "[st %d] %s" % (count, original_subject)
+    else:
+        return "%s[st %d]%s" % (m.group(1), count, m.group(3))
 
 
 @route(lameserv_endpoint_address)
@@ -11,7 +20,7 @@ def START(message, address=None, host=None):
     if not subs.is_subscriber(message["From"]):
         return
     
-    subj = "[st: %d] %s" % (threadcounter.ThreadCounter().count(), message["Subject"])
+    subj = build_subject(threadcounter.ThreadCounter().count(), message["Subject"])
     
     for sub in subs:
         resp = mail.MailResponse(To=sub, From=message["From"], Subject=subj, Body=message.body())
@@ -21,3 +30,6 @@ def START(message, address=None, host=None):
         resp["To"] = lameserv_endpoint_address
         relay.deliver(resp.to_message(), To=sub, From=lameserv_endpoint_address)
 
+@route("(address)@(host)", address=".+")
+def NOPE(message, address=None, host=None):
+    raise SMTPError(11)
