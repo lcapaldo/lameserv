eb14483ddd44fb6634f24443711c8f9a5a1d67b9
Casey Kolderup <casey@kolderup.org>
2012-10-15 21:19:43 -0400

    trailing whitespace grr

--- app/handlers/forwarder.py
+++ app/handlers/forwarder.py
@@ -10,13 +10,13 @@ def START(message, address=None, host=None):
     subs = subscribers.Subscribers()
     if not subs.is_subscriber(message["From"]):
         return
-    
+
     subj = "[st: %d] %s" % (threadcounter.ThreadCounter().count(), message["Subject"])
-    
+
     for sub in subs:
         resp = mail.MailResponse(To=sub, From=message["From"], Subject=subj, Body=message.body())
         resp.attach_all_parts(message)
-        resp["Reply-To"] = lameserv_endpoint_address 
+        resp["Reply-To"] = lameserv_endpoint_address
         resp["Sender"] = lameserv_endpoint_address
         resp["To"] = lameserv_endpoint_address
         relay.deliver(resp.to_message(), To=sub, From=lameserv_endpoint_address)
